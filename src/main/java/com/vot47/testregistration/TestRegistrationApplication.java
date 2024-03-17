b8839540059e2bec4aa687a663567e64d038d675
Denys <dendet94@gmail.com>
2024-03-17 18:46:57 +0200

    application init

---
+++ src/main/java/com/vot47/testregistration/TestRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.vot47.testregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vot47/testregistration/TestRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vot47.testregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
